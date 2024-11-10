ded023e8a47bee30ac7d7ee63da832b932a315ae
Sujan Ghimire <ghimire.amrit147@gmail.com>
2024-11-10 16:01:17 +0545

    bug: minor session bug resolved

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -86,16 +86,20 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  const { databases, account } = await createSessionClient();
+  try {
+    const { databases, account } = await createSessionClient();
 
-  const result = await account.get();
-  const user = await databases.listDocuments(
-    appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId,
-    [Query.equal('accountId', result.$id)]
-  );
-  if (user.total <= 0) return null;
-  return parseStringify(user.documents[0]);
+    const result = await account.get();
+    const user = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal('accountId', result.$id)]
+    );
+    if (user.total <= 0) return null;
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const signOutUser = async () => {
